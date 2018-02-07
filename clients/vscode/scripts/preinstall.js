35bcacd21c666a8db0061597fd54cf35968ef705
Aurélien Pupier <apupier@redhat.com>
2018-02-07 12:11:09 +0100

    #4 - retrieve the Language server jar and not an old vsix
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- clients/vscode/scripts/preinstall.js
+++ clients/vscode/scripts/preinstall.js
@@ -1,7 +1,7 @@
 'use strict';
 const fs = require('fs');
 const download = require('download')
-var url = "https://github.com/lhein/camel-language-server/releases/download/untagged-d42064681113e838bd59/vscode-apache-camel-0.0.1.vsix"
+var url = "https://github.com/lhein/camel-language-server/releases/download/untagged-d42064681113e838bd59/camel-lsp-server-1.0.0-SNAPSHOT.jar"
 
 download(url).then(data => {
     fs.writeFileSync('./jars/language-server.jar', data);
