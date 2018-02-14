b9ae927fb044e8900e5d515803f219b70846a491
Aurélien Pupier <apupier@redhat.com>
2018-02-14 12:08:26 +0100

    #4 - Update to latest server jar
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- clients/vscode/scripts/preinstall.js
+++ clients/vscode/scripts/preinstall.js
@@ -1,7 +1,7 @@
 'use strict';
 const fs = require('fs');
 const download = require('download')
-var url = "https://github.com/lhein/camel-language-server/releases/download/untagged-d42064681113e838bd59/camel-lsp-server-1.0.0-SNAPSHOT.jar"
+var url = "https://github.com/lhein/camel-language-server/releases/download/untagged-91e507435585e8ecfacd/camel-lsp-server-1.0.0-SNAPSHOT.jar"
 
 download(url).then(data => {
     fs.writeFileSync('./jars/language-server.jar', data);
