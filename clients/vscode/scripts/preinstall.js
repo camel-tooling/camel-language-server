b85ea709af2d6a1e509c1e1898dd08e9b20d8a35
Aurélien Pupier <apupier@redhat.com>
2018-03-15 16:17:06 +0100

    Use the newly available server on apupier organization

--- clients/vscode/scripts/preinstall.js
+++ clients/vscode/scripts/preinstall.js
@@ -1,7 +1,7 @@
 'use strict';
 const fs = require('fs');
 const download = require('download')
-var url = "https://github.com/apupier/camel-language-server/releases/download/untagged-91e507435585e8ecfacd/camel-lsp-server-1.0.0-SNAPSHOT.jar"
+var url = "https://github.com/apupier/camel-language-server/releases/download/untagged-5a182c0119681071de73/camel-lsp-server-1.0.0-SNAPSHOT.jar"
 
 download(url).then(data => {
     fs.writeFileSync('./jars/language-server.jar', data);
