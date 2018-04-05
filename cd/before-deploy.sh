de5333d7dc7a23989a50c5b94bb9bb4c409bf141
Lars Heinemann <lhein.smx@gmail.com>
2018-04-05 10:12:13 +0200

    enable deployment from travis to central
    Signed-off-by: Lars Heinemann <lhein.smx@gmail.com>

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 mv ~/.gnupg ~/dot.gnupg  
-#openssl aes-256-cbc -K $encrypted_5b0511f84952_key -iv $encrypted_5b0511f84952_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
-gpg --fast-import cd/codesigning.asc
+openssl aes-256-cbc -K $encrypted_5b0511f84952_key -iv $encrypted_5b0511f84952_iv -in $GPG_DIR/codesigning.gpg.enc -out $GPG_DIR/codesigning.gpg -d
+gpg --import $GPG_DIR/codesigning.gpg
