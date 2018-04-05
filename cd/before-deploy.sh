a3bf936910b33d7d55e1f79f97b0d406d2f6f0b4
Lars Heinemann <lhein.smx@gmail.com>
2018-04-05 16:02:40 +0200

    improved deployment
    Signed-off-by: Lars Heinemann <lhein.smx@gmail.com>

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_5b0511f84952_key -iv $encrypted_5b0511f84952_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
+    print $SALT > cd/codesigning.asc
     gpg --fast-import cd/codesigning.asc
 fi
 
