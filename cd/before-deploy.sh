2fe55f8d3a1508d3c70971c12169df316f391d83
Lars Heinemann <lhein.smx@gmail.com>
2018-04-05 22:51:54 +0200

    another try
    Signed-off-by: Lars Heinemann <lhein.smx@gmail.com>

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -a -k $PP -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
+    openssl aes-256-ecb -k $PP -in cd/codesigning.asc.enc -out cd/codesigning.asc -d -v
     gpg --batch --fast-import cd/codesigning.asc
 fi
 
