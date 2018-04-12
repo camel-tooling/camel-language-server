2980193ef8443e63db88b7f490ba8d62b679058a
Lars Heinemann <lhein.smx@gmail.com>
2018-04-12 15:14:43 +0200

    cleanup and passphrase regen
    Signed-off-by: Lars Heinemann <lhein.smx@gmail.com>

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
-export DEC_TOKEN=$1
 mv ~/.gnupg ~/dot.gnupg  
-
 openssl aes-256-cbc -K $encrypted_5b0511f84952_key -iv $encrypted_5b0511f84952_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
 gpg --fast-import cd/codesigning.asc
-
-
-#openssl enc -aes-256-cbc -d -in $GPG_DIR/pubring.gpg.enc -out $GPG_DIR/pubring.gpg -k $DEC_TOKEN
-#openssl enc -aes-256-cbc -d -in $GPG_DIR/secring.gpg.enc -out $GPG_DIR/secring.gpg -k $DEC_TOKEN
-#gpg --import $GPG_DIR/pubring.gpg
-#gpg --import $GPG_DIR/secring.gpg
--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    mvn deploy -DskipTests -P sign,build-extras --settings cd/mvnsettings.xml -X
+    mvn deploy -DskipTests -P sign,build-extras --settings cd/mvnsettings.xml
 fi
 
