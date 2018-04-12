81f4e387ed295f33f5931ea0dd11a3bffad23b5d
Lars Heinemann <lhein.smx@gmail.com>
2018-04-12 12:29:48 +0200

    enabled deploy debug outputs

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    mvn deploy -P sign,build-extras --settings cd/mvnsettings.xml
+    mvn deploy -DskipTests -P sign,build-extras --settings cd/mvnsettings.xml -X
 fi
 
