8d1a9281d26655bd51a9b9a68cce8bc903411d05
Lars Heinemann <lhein.smx@gmail.com>
2018-04-12 12:48:39 +0200

    Update before-deploy.sh

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -3,5 +3,5 @@ export DEC_TOKEN=$1
 openssl enc -aes-256-cbc -d -in $GPG_DIR/pubring.gpg.enc -out $GPG_DIR/pubring.gpg -k $DEC_TOKEN
 openssl enc -aes-256-cbc -d -in $GPG_DIR/secring.gpg.enc -out $GPG_DIR/secring.gpg -k $DEC_TOKEN
 mv ~/.gnupg ~/dot.gnupg  
-gpg —import $GPG_DIR/pubring.gpg
-gpg —import $GPG_DIR/secring.gpg
+gpg --import $GPG_DIR/pubring.gpg
+gpg --import $GPG_DIR/secring.gpg
