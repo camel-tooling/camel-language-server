33a9362121fd0dfaad7efa767877644d73ac5c6a
Lars Heinemann <lhein.smx@gmail.com>
2018-04-12 11:53:37 +0200

    fix issue in before-deploy script with env var ref
    Signed-off-by: Lars Heinemann <lhein.smx@gmail.com>

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
-#export DEC_TOKEN=$1
-echo $GPG_DIR
-openssl enc -aes-256-cbc -d -in cd/pubring.gpg.enc -out cd/pubring.gpg -k $DEC_TOKEN
-openssl enc -aes-256-cbc -d -in cd/secring.gpg.enc -out cd/secring.gpg -k $DEC_TOKEN
-#openssl aes-256-cbc -k $OPENSSLPASS -in $GPG_DIR/pubring.gpg.enc -out $GPG_DIR/pubring.gpg -d -v
-#openssl aes-256-cbc -k $OPENSSLPASS -in $GPG_DIR/secring.gpg.enc -out $GPG_DIR/secring.gpg -d -v
-
+export DEC_TOKEN=$1
+openssl enc -aes-256-cbc -d -in $GPG_DIR/pubring.gpg.enc -out $GPG_DIR/pubring.gpg -k $DEC_TOKEN
+openssl enc -aes-256-cbc -d -in $GPG_DIR/secring.gpg.enc -out $GPG_DIR/secring.gpg -k $DEC_TOKEN
