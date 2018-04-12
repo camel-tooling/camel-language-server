ac48a6e82ca953981a8864288dd6cbf6324e2fc2
Lars Heinemann <lhein.smx@gmail.com>
2018-04-12 15:36:16 +0200

    passphrase regen
    Signed-off-by: Lars Heinemann <lhein.smx@gmail.com>

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 mv ~/.gnupg ~/dot.gnupg  
-openssl aes-256-cbc -K $encrypted_5b0511f84952_key -iv $encrypted_5b0511f84952_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
+#openssl aes-256-cbc -K $encrypted_5b0511f84952_key -iv $encrypted_5b0511f84952_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
 gpg --fast-import cd/codesigning.asc
