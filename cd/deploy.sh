59db60f3099d498a205ab7ba729dbaab8d3d35ce
Lars Heinemann <lhein.smx@gmail.com>
2018-06-04 11:19:46 +0200

    fix scripts to enable clean releases
    Signed-off-by: Lars Heinemann <lhein.smx@gmail.com>

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    mvn deploy -DskipTests -P sign,build-extras --settings cd/mvnsettings.xml
-fi
+mvn deploy -DskipTests -P sign,build-extras --settings cd/mvnsettings.xml
 
