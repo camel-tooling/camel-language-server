7ec090aa514f8f2eccc60560fa8bfa99f478f4c4
Aurélien Pupier <apupier@redhat.com>
2018-09-07 10:02:46 +0200

    Upgrade to Camel 2.22.1 (#138)
    - upgrade Camel version
    - activate the 2 tests awaiting fix upstream for reference and
    diagnostic when a camel namespace prefix is used (and fixed tests and
    implementation on our side too...)
    
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- src/test/java/com/github/cameltooling/lsp/internal/diagnostic/CamelDiagnosticTest.java
+++ src/test/java/com/github/cameltooling/lsp/internal/diagnostic/CamelDiagnosticTest.java
@@ -25,7 +25,6 @@ import java.io.FileNotFoundException;
 import org.eclipse.lsp4j.DidSaveTextDocumentParams;
 import org.eclipse.lsp4j.Range;
 import org.eclipse.lsp4j.TextDocumentIdentifier;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import com.github.cameltooling.lsp.internal.AbstractCamelLanguageServerTest;
@@ -49,14 +48,13 @@ public class CamelDiagnosticTest extends AbstractCamelLanguageServerTest {
 	}
 	
 	@Test
-	@Ignore("blocked by upstream issue https://issues.apache.org/jira/browse/CAMEL-12735")
 	public void testValidationErrorWithNamespacePrefix() throws Exception {
 		testDiagnostic("camel-with-endpoint-error-withNamespacePrefix", 1, ".xml");
 		Range range = lastPublishedDiagnostics.getDiagnostics().get(0).getRange();
 		assertThat(range.getStart().getLine()).isEqualTo(8);
 		assertThat(range.getStart().getCharacter()).isEqualTo(0);
 		assertThat(range.getEnd().getLine()).isEqualTo(8);
-		assertThat(range.getEnd().getCharacter()).isEqualTo(52);
+		assertThat(range.getEnd().getCharacter()).isEqualTo(51);
 	}
 	
 	@Test
