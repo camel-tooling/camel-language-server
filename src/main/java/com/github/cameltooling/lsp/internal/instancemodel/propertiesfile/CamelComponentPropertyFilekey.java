fffc41f53649bc9c752be8f67a10cb2b8d1fffa8
Aurélien Pupier <apupier@redhat.com>
2019-12-10 16:53:34 +0100

    FUSETOOLS2-82 - provide Component properties completion in properties file
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

---
+++ src/main/java/com/github/cameltooling/lsp/internal/instancemodel/propertiesfile/CamelComponentPropertyFilekey.java
@@ -0,0 +1,67 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.cameltooling.lsp.internal.instancemodel.propertiesfile;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import org.apache.camel.catalog.CamelCatalog;
+import org.eclipse.lsp4j.CompletionItem;
+
+import com.github.cameltooling.lsp.internal.completion.CamelComponentIdsCompletionsFuture;
+import com.github.cameltooling.lsp.internal.completion.CamelComponentOptionNamesCompletionFuture;
+
+/**
+ * Represents the subpart component key.
+ * For instance, with "camel.component.timer.delay=1000",
+ * it is used to represents "timer.delay"
+ * 
+ */
+public class CamelComponentPropertyFilekey {
+
+	private CompletableFuture<CamelCatalog> camelCatalog;
+	private String fullCamelComponentPropertyFileKey;
+	private String componentId;
+
+	public CamelComponentPropertyFilekey(CompletableFuture<CamelCatalog> camelCatalog, String camelComponentPropertyFileKey) {
+		this.camelCatalog = camelCatalog;
+		this.fullCamelComponentPropertyFileKey = camelComponentPropertyFileKey;
+		int firstDotIndex = camelComponentPropertyFileKey.indexOf('.');
+		if(firstDotIndex != -1) {
+			componentId = camelComponentPropertyFileKey.substring(0, firstDotIndex);
+		} else {
+			componentId = camelComponentPropertyFileKey;
+		}
+	}
+
+	public boolean isInRange(int positionChar) {
+		return CamelPropertyFileKeyInstance.CAMEL_COMPONENT_KEY_PREFIX.length() <= positionChar
+				&& positionChar <= fullCamelComponentPropertyFileKey.length() + CamelPropertyFileKeyInstance.CAMEL_COMPONENT_KEY_PREFIX.length();
+	}
+
+	public CompletableFuture<List<CompletionItem>> getCompletions(int positionChar) {
+		if(CamelPropertyFileKeyInstance.CAMEL_COMPONENT_KEY_PREFIX.length() == positionChar) {
+			return camelCatalog.thenApply(new CamelComponentIdsCompletionsFuture());
+		} else if(componentId != null && CamelPropertyFileKeyInstance.CAMEL_COMPONENT_KEY_PREFIX.length() + componentId.length() + 1 == positionChar){
+			return camelCatalog.thenApply(new CamelComponentOptionNamesCompletionFuture(componentId));
+		} else {
+			return CompletableFuture.completedFuture(Collections.emptyList());
+		}
+	}
+
+}
---
+++ src/test/java/com/github/cameltooling/lsp/internal/completion/camelapplicationproperties/CamelPropertiesComponentOptionNameCompletionTest.java
@@ -0,0 +1,105 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.cameltooling.lsp.internal.completion.camelapplicationproperties;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.net.URISyntaxException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import org.eclipse.lsp4j.CompletionItem;
+import org.eclipse.lsp4j.CompletionList;
+import org.eclipse.lsp4j.InitializeParams;
+import org.eclipse.lsp4j.Position;
+import org.eclipse.lsp4j.TextDocumentItem;
+import org.eclipse.lsp4j.jsonrpc.messages.Either;
+import org.junit.jupiter.api.Test;
+
+import com.github.cameltooling.lsp.internal.AbstractCamelLanguageServerTest;
+import com.github.cameltooling.lsp.internal.CamelLanguageServer;
+import com.google.gson.Gson;
+
+public class CamelPropertiesComponentOptionNameCompletionTest extends AbstractCamelLanguageServerTest {
+	
+	@Test
+	public void testProvideCompletion() throws Exception {
+		CompletableFuture<Either<List<CompletionItem>, CompletionList>> completions = retrieveCompletion(new Position(0, 27));
+		
+		assertThat(completions.get().getLeft()).hasSize(1);
+	}
+	
+	@Test
+	public void testProvideNoCompletionAfterComponentproperty() throws Exception {
+		CompletableFuture<Either<List<CompletionItem>, CompletionList>> completions = retrieveCompletion(new Position(0, 46));
+		
+		assertThat(completions.get().getLeft()).isEmpty();
+	}
+		
+	protected CompletableFuture<Either<List<CompletionItem>, CompletionList>> retrieveCompletion(Position position) throws URISyntaxException, InterruptedException, ExecutionException {
+		String fileName = "a.properties";
+		CamelLanguageServer camelLanguageServer = initializeLanguageServer(fileName, new TextDocumentItem(fileName, CamelLanguageServer.LANGUAGE_ID, 0, "camel.component.acomponent.aComponentProperty."));
+		return getCompletionFor(camelLanguageServer, position, fileName);
+	}
+	
+	@Override
+	protected InitializeParams getInitParams() throws URISyntaxException {
+		InitializeParams initParams = super.getInitParams();
+		String component = "{\n" + 
+				" \"component\": {\n" + 
+				"    \"kind\": \"component\",\n" + 
+				"    \"scheme\": \"acomponent\",\n" + 
+				"    \"syntax\": \"acomponent:withsyntax\",\n" + 
+				"    \"title\": \"A Component\",\n" + 
+				"    \"description\": \"Description of my component.\",\n" + 
+				"    \"label\": \"\",\n" + 
+				"    \"deprecated\": true,\n" + 
+				"    \"deprecationNote\": \"\",\n" + 
+				"    \"async\": false,\n" + 
+				"    \"consumerOnly\": true,\n" + 
+				"    \"producerOnly\": false,\n" + 
+				"    \"lenientProperties\": false,\n" + 
+				"    \"javaType\": \"org.test.AComponent\",\n" + 
+				"    \"firstVersion\": \"1.0.0\",\n" + 
+				"    \"groupId\": \"org.test\",\n" + 
+				"    \"artifactId\": \"camel-acomponent\",\n" + 
+				"    \"version\": \"3.0.0\"\n" + 
+				"  },\n" + 
+				"  \"componentProperties\": {\n" + 
+				"\"aComponentProperty\": { \"kind\": \"parameter\", \"displayName\": \"A Component property \", \"group\": \"common\", \"required\": false, \"type\": \"string\", \"javaType\": \"java.lang.String\", \"deprecated\": false, \"secret\": false, \"defaultValue\": \"org.apache.camel.event\", \"configurationClass\": \"org.apache.camel.component.knative.KnativeConfiguration\", \"configurationField\": \"configuration\", \"description\": \"A parameter description\" }\n" + 
+				"  },\n" + 
+				"  \"properties\": {\n" +
+				"  }\n" + 
+				"}";
+		Map<Object, Object> initializationOptions = createMapSettingsWithComponent(component);
+		initParams.setInitializationOptions(initializationOptions);
+		return initParams;
+	}
+	
+	private Map<Object, Object> createMapSettingsWithComponent(String component) {
+		Map<Object, Object> camelIntializationOptions = new HashMap<>();
+		camelIntializationOptions.put("extra-components", Collections.singletonList(new Gson().fromJson(component, Map.class)));
+		HashMap<Object, Object> initializationOptions = new HashMap<>();
+		initializationOptions.put("camel", camelIntializationOptions);
+		return initializationOptions;
+	}
+	
+}
