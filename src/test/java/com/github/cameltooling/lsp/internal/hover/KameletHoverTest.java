e9b012991e79ab4eb3b8820f3fc07f19e5327801
Aurélien Pupier <apupier@redhat.com>
2021-06-18 15:58:57 +0200

    use parameterized test to simply reading and maintenance
    see https://sonarcloud.io/project/issues?id=camel-lsp-server&issues=AXoZ9PK8TKya_KffcyJS&open=AXoZ9PK8TKya_KffcyJS
    
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- src/test/java/com/github/cameltooling/lsp/internal/hover/KameletHoverTest.java
+++ src/test/java/com/github/cameltooling/lsp/internal/hover/KameletHoverTest.java
@@ -18,47 +18,45 @@ package com.github.cameltooling.lsp.internal.hover;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.net.URISyntaxException;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Stream;
 
 import org.eclipse.lsp4j.Hover;
 import org.eclipse.lsp4j.HoverParams;
 import org.eclipse.lsp4j.Position;
 import org.eclipse.lsp4j.TextDocumentIdentifier;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import com.github.cameltooling.lsp.internal.AbstractCamelLanguageServerTest;
 import com.github.cameltooling.lsp.internal.CamelLanguageServer;
 
 class KameletHoverTest extends AbstractCamelLanguageServerTest {
 
-	@Test
-	void testProvideDocumentationOnHover() throws Exception {
-		CamelLanguageServer camelLanguageServer = initializeLanguageServer("<from uri=\"kamelet:aws-ddb-streams-source\" xmlns=\"http://camel.apache.org/schema/spring\"></from>\n");
+	@ParameterizedTest(name = "{0}")
+	@MethodSource
+	void testHoverDocumentation(String testName, String camelUri, int characterPositionInCamelUriToTest, String expectedHoverContent)
+			throws URISyntaxException, InterruptedException, ExecutionException {
+		String beforeCamelUriToTest = "<from uri=\"";
+		CamelLanguageServer camelLanguageServer = initializeLanguageServer(beforeCamelUriToTest+camelUri+"\" xmlns=\"http://camel.apache.org/schema/spring\"></from>\n");
 		
-		HoverParams hoverParams = new HoverParams(new TextDocumentIdentifier(DUMMY_URI+".xml"), new Position(0, 26));
+		HoverParams hoverParams = new HoverParams(new TextDocumentIdentifier(DUMMY_URI+".xml"), new Position(0, beforeCamelUriToTest.length() + characterPositionInCamelUriToTest));
 		CompletableFuture<Hover> hover = camelLanguageServer.getTextDocumentService().hover(hoverParams);
 		
-		assertThat(hover.get().getContents().getLeft().get(0).getLeft()).isEqualTo("Receive events from AWS DynamoDB Streams.");
+		assertThat(hover.get().getContents().getLeft().get(0).getLeft()).isEqualTo(expectedHoverContent);
 	}
-	
-	@Test
-	void testProvideGenericDocumentationOnHoverOnUnknownKamelet() throws Exception {
-		CamelLanguageServer camelLanguageServer = initializeLanguageServer("<from uri=\"kamelet:unknown\" xmlns=\"http://camel.apache.org/schema/spring\"></from>\n");
 		
-		HoverParams hoverParams = new HoverParams(new TextDocumentIdentifier(DUMMY_URI+".xml"), new Position(0, 19));
-		CompletableFuture<Hover> hover = camelLanguageServer.getTextDocumentService().hover(hoverParams);
-		
-		assertThat(hover.get().getContents().getLeft().get(0).getLeft()).isEqualTo("kamelet:templateId/routeId");
+	static Stream<Arguments> testHoverDocumentation() {
+	    return Stream.of(
+	      Arguments.of("Provide Kamelet documentation on kamelet template id",
+	    		  "kamelet:aws-ddb-streams-source", 15, "Receive events from AWS DynamoDB Streams."),
+	      Arguments.of("Provide generic documentation on hover on unknown kamelet",
+	    		  "kamelet:unknown", 8, "kamelet:templateId/routeId"),
+	      Arguments.of("Provide documentation on hover on known kamelet property name",
+	    		  "kamelet:aws-ddb-streams-source?secretKey=", 39, "The secret key obtained from AWS")
+	    );
 	}
-	
-	@Test
-	void testProvideDocumentationOnHoverOnKnownKameletPropertyName() throws Exception {
-		CamelLanguageServer camelLanguageServer = initializeLanguageServer("<from uri=\"kamelet:aws-ddb-streams-source?secretKey=\" xmlns=\"http://camel.apache.org/schema/spring\"></from>\n");
-		
-		HoverParams hoverParams = new HoverParams(new TextDocumentIdentifier(DUMMY_URI+".xml"), new Position(0, 50));
-		CompletableFuture<Hover> hover = camelLanguageServer.getTextDocumentService().hover(hoverParams);
-		
-		assertThat(hover.get().getContents().getLeft().get(0).getLeft()).isEqualTo("The secret key obtained from AWS");
-	}
-	
 }
