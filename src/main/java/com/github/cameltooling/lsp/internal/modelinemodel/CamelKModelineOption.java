b94c2f768b12fc3926921ebac5cf769eaa3855db
Aurélien Pupier <apupier@redhat.com>
2021-07-01 10:05:25 +0200

    FUSETOOLS2-1187 - completion for config=file: based on local filesystem
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- src/main/java/com/github/cameltooling/lsp/internal/modelinemodel/CamelKModelineOption.java
+++ src/main/java/com/github/cameltooling/lsp/internal/modelinemodel/CamelKModelineOption.java
@@ -68,7 +68,7 @@ public class CamelKModelineOption implements ILineRangeDefineable {
 			} else if(CamelKModelineOptionNames.OPTION_NAME_OPEN_API.equals(optionName)) {
 				return new CamelKModelineOpenAPIOption(value, startPosition, documentItem.getUri(), line);
 			} else if(CamelKModelineOptionNames.OPTION_NAME_CONFIG.equals(optionName)) {
-				return new CamelKModelineConfigOption(value, startPosition, line);
+				return new CamelKModelineConfigOption(value, startPosition, documentItem.getUri(), line);
 			} else {
 				return new GenericCamelKModelineOptionValue(value, startPosition, line);
 			}
--- src/test/java/com/github/cameltooling/lsp/internal/completion/modeline/CamelKModelineConfigOptionTest.java
+++ src/test/java/com/github/cameltooling/lsp/internal/completion/modeline/CamelKModelineConfigOptionTest.java
@@ -18,22 +18,31 @@ package com.github.cameltooling.lsp.internal.completion.modeline;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 import org.eclipse.lsp4j.CompletionItem;
 import org.eclipse.lsp4j.CompletionList;
 import org.eclipse.lsp4j.Position;
+import org.eclipse.lsp4j.Range;
+import org.eclipse.lsp4j.TextEdit;
 import org.eclipse.lsp4j.jsonrpc.messages.Either;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 import com.github.cameltooling.lsp.internal.AbstractCamelLanguageServerTest;
 import com.github.cameltooling.lsp.internal.CamelLanguageServer;
+import com.google.common.io.Files;
 
 class CamelKModelineConfigOptionTest extends AbstractCamelLanguageServerTest {
-		
+	
+	@TempDir
+	File temporaryDir;
+	
 	@Test
-	void testProvideCompletionForComponentSuffix() throws Exception {
+	void testProvideCompletionForConfigKind() throws Exception {
 		String text = "// camel-k: config=";
 		CamelLanguageServer camelLanguageServer = initializeLanguageServer(text);
 		
@@ -41,6 +50,61 @@ class CamelKModelineConfigOptionTest extends AbstractCamelLanguageServerTest {
 		
 		List<CompletionItem> completionItems = completions.get().getLeft();
 		assertThat(completionItems.stream().map(completionItem -> completionItem.getLabel())).containsOnly("configmap:", "secret:", "file:");
+	}
+	
+	@Test
+	void testProvideCompletionForFile() throws Exception {
+		String modeline = "# camel-k: config=file:";
+		File camelKfile = createFileStructureForTest(modeline);
+		
+		CamelLanguageServer camelLanguageServer = initializeLanguageServer(camelKfile);
+		
+		CompletableFuture<Either<List<CompletionItem>, CompletionList>> completions = getCompletionFor(camelLanguageServer, new Position(0, modeline.length()), camelKfile.toURI().toString());
+		
+		List<CompletionItem> completionItems = completions.get().getLeft();
+		assertThat(completionItems).containsOnly(
+				createExpectedCompletionItem("a.properties", modeline.length()),
+				createExpectedCompletionItem("myFolder"+File.separator+"aSecond.properties", modeline.length()),
+				createExpectedCompletionItem("anotherFile.txt", modeline.length()));
+	}
+	
+	private CompletionItem createExpectedCompletionItem(String text, int endPosition) {
+		CompletionItem siblingCompletionItem = new CompletionItem(text);
+		siblingCompletionItem.setTextEdit(Either.forLeft(new TextEdit(new Range(new Position(0, endPosition), new Position(0, endPosition)), text)));
+		return siblingCompletionItem;
+	}
+	
+	/**
+	 * temporaryDir
+	 *   | test.camelk.yaml
+	 *   | anotherFile.txt
+	 *   | a.properties
+	 *   | myFolder
+	 *   --| aSecond.properties
+	 *   | .vscode
+	 *   --| shouldbefiltered.txt
+	 *   | .settings
+	 *   --| shouldbefiltered2.txt
+	 *   | .theia
+	 *   --| shouldbefiltered3.txt
+	 * 
+	 * @return The Camel K yaml file created at the root of the temporary directory
+	 * @throws IOException
+	 */
+	private File createFileStructureForTest(String modeline) throws IOException {
+		File camelKfile = new File(temporaryDir, "test.camelk.yaml");
+		Files.write(modeline.getBytes(), camelKfile);
+		File aSiblingPropertyFile = new File(temporaryDir, "a.properties");
+		aSiblingPropertyFile.createNewFile();
+		
+		createFolderWithFile("myFolder", "aSecond.properties", temporaryDir);
+		createFolderWithFile(".vscode", "shouldbefiltered.txt", temporaryDir);
+		createFolderWithFile(".settings", "shouldbefiltered2.txt", temporaryDir);
+		createFolderWithFile(".theia", "shouldbefiltered3.txt", temporaryDir);
+		
+		File anotherFile = new File(temporaryDir, "anotherFile.txt");
+		anotherFile.createNewFile();
+		return camelKfile;
 	}
 	
 }
