627a111ec7e187dc49391a2ca5fdbd31ea2c5790
Aurélien Pupier <apupier@redhat.com>
2021-07-01 15:08:52 +0200

    FUSETOOLS2-1182 - Adapt resource modeline option to new API
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- src/main/java/com/github/cameltooling/lsp/internal/modelinemodel/CamelKModelineResourceOption.java
+++ src/main/java/com/github/cameltooling/lsp/internal/modelinemodel/CamelKModelineResourceOption.java
@@ -16,24 +16,75 @@
  */
 package com.github.cameltooling.lsp.internal.modelinemodel;
 
-import java.nio.file.Path;
-import java.util.function.Predicate;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
-import com.github.cameltooling.lsp.internal.completion.modeline.CamelKModelineOptionNames;
+import org.apache.camel.catalog.CamelCatalog;
+import org.eclipse.lsp4j.CompletionItem;
 
-public class CamelKModelineResourceOption extends CamelKModelineLocalResourceRelatedOption {
+public class CamelKModelineResourceOption implements ICamelKModelineOptionValue {
 	
-	public CamelKModelineResourceOption(String value, int startPosition, String documentItemUri, int line) {
-		super(value, startPosition, documentItemUri, line);
+	private static final String PREFIX_FILE = "file:";
+
+	private String value;
+	private int startPosition;
+	private int line;
+	private CamelKModelineResourceFileOption resourceFileValue;
+
+	public CamelKModelineResourceOption(String value, int startPosition, String uri, int line) {
+		this.value = value;
+		this.startPosition = startPosition;
+		this.line = line;
+		if(value.startsWith(PREFIX_FILE)) {
+			int endPosition = value.indexOf('@');
+			if(endPosition != -1) {
+				this.resourceFileValue = new CamelKModelineResourceFileOption(value.substring(PREFIX_FILE.length(), endPosition), startPosition + PREFIX_FILE.length(), uri, line);
+			} else {
+				this.resourceFileValue = new CamelKModelineResourceFileOption(value.substring(PREFIX_FILE.length()), startPosition + PREFIX_FILE.length(), uri, line);
+			}
+		}
 	}
 
 	@Override
-	protected String getPropertyName() {
-		return  CamelKModelineOptionNames.OPTION_NAME_RESOURCE;
+	public int getLine() {
+		return line;
 	}
 
 	@Override
-	protected Predicate<Path> getFilter() {
-		return path -> true;
+	public int getStartPositionInLine() {
+		return startPosition;
 	}
+
+	@Override
+	public int getEndPositionInLine() {
+		return startPosition + value.length();
+	}
+
+	@Override
+	public String getValueAsString() {
+		return value;
+	}
+	
+	@Override
+	public CompletableFuture<List<CompletionItem>> getCompletions(int position, CompletableFuture<CamelCatalog> camelCatalog) {
+		if(position == startPosition) {
+			CompletionItem configmap = new CompletionItem("configmap:");
+			configmap.setDocumentation("Add a runtime resource from a Configmap (syntax: configmap:name[/key][@path], "
+					+ "where name represents the configmap name, "
+					+ "key optionally represents the configmap key to be filtered and path represents the destination path)");
+			CompletionItem secret = new CompletionItem("secret:");
+			secret.setDocumentation("Add a runtime resource from a Secret (syntax: secret:name[/key][@path], "
+					+ "where name represents the secret name, "
+					+ "key optionally represents the secret key to be filtered and path represents the destination path)");
+			CompletionItem file = new CompletionItem(PREFIX_FILE);
+			file.setDocumentation("Add a runtime resource from a file (syntax: file:name[@path], "
+					+ "where name represents the local file path and path represents the destination path)");
+			return CompletableFuture.completedFuture(Arrays.asList(configmap, secret, file));
+		} else if(resourceFileValue != null && resourceFileValue.isInRange(position)) {
+			return resourceFileValue.getCompletions(position, camelCatalog);
+		}
+		return ICamelKModelineOptionValue.super.getCompletions(position, camelCatalog);
+	}
+
 }
