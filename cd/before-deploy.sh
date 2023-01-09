53c3b331cee0a6b1485cd13525f17677d422c406
Aurélien Pupier <apupier@redhat.com>
2023-01-09 17:08:15 +0100

    FUSETOOLS2-1912 - Configure sign and deploy from GitHub Actions
    - The Maven central token has been made completely configured from
    GitHub secrets. Created from my Maven Central Nexus account as described
    in https://github.com/chhh/sonatype-ossrh-parent/blob/master/publishing-to-maven-central.md#configuring-maven-to-know-the-credentials-for-sonatype-servers
    - GPG keys and passphrase are configurable, currently configured on Ci
    from my own account (apupier@redhat.com)
    
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- cd/before-deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-mv ~/.gnupg ~/dot.gnupg  
-openssl aes-256-cbc -K $encrypted_5b0511f84952_key -iv $encrypted_5b0511f84952_iv -in $GPG_DIR/codesigning.gpg.enc -out $GPG_DIR/codesigning.gpg -d
-gpg --batch --import $GPG_DIR/codesigning.gpg
