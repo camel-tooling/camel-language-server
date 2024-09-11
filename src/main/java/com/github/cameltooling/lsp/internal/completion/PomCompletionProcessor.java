bd2c27a64e8ceca2cdaeedd5eeb268c774bc6b3d
Aurélien Pupier <apupier@redhat.com>
2024-09-11 20:32:13 +0200

    FUSETOOLS2-2478 - completion for dependencies of Camel component in pom
    there will be completion provided sometimes not at the exact right-place
    but should be fine for now.
    
    to have a good ones in this iteration the user will have to use the
    correct runtime provider otherwise it will end up with not specific to
    their runtime dependency. To improve in the future by helping to set the
    runtime provider or to look to the content of the pom.xml
    
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- src/main/java/com/github/cameltooling/lsp/internal/completion/PomCompletionProcessor.java
+++ src/main/java/com/github/cameltooling/lsp/internal/completion/PomCompletionProcessor.java
@@ -16,37 +16,125 @@
  */
 package com.github.cameltooling.lsp.internal.completion;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
+import org.apache.camel.catalog.CamelCatalog;
 import org.eclipse.lsp4j.CompletionItem;
+import org.eclipse.lsp4j.Position;
+import org.eclipse.lsp4j.TextDocumentItem;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.cameltooling.lsp.internal.catalog.model.ComponentModel;
+import com.github.cameltooling.lsp.internal.catalog.util.ModelHelper;
 
 public class PomCompletionProcessor {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(PomCompletionProcessor.class);
 
-	public CompletableFuture<List<CompletionItem>> getCompletions() {
+	private TextDocumentItem textDocumentItem;
+	private CompletableFuture<CamelCatalog> camelCatalog;
+
+	public PomCompletionProcessor(TextDocumentItem textDocumentItem, CompletableFuture<CamelCatalog> camelCatalog) {
+		this.textDocumentItem = textDocumentItem;
+		this.camelCatalog = camelCatalog;
+	}
+
+	public CompletableFuture<List<CompletionItem>> getCompletions(Position position) {
+		List<CompletionItem> completions = new ArrayList<>();
+		String text = textDocumentItem.getText();
+		String[] lines = text.split("\\R");
+		if (isInsideTag(position, lines, "profiles")) {
+			completions.add(createCamelQuarkusDebugProfileCompletionItem());
+		}
+		
+		if (isInsideTag(position, lines, "dependencies")) {
+			try {
+				completions.addAll(createCamelDependenciesCompletionItems());
+			} catch (Exception e) {
+				LOGGER.warn("Error while computing Camel dependencies for Maven pom", e);
+			}
+		}
+		
+		return CompletableFuture.completedFuture(completions);
+	}
+
+	private List<CompletionItem> createCamelDependenciesCompletionItems() throws InterruptedException, ExecutionException {
+		List<CompletionItem> completions = new ArrayList<>();
+		List<String> componentNames = camelCatalog.get().findComponentNames();
+		for (String componentName : componentNames) {
+			ComponentModel componentModel = ModelHelper.generateComponentModel(camelCatalog.get().componentJSonSchema(componentName), false);
+			CompletionItem completionItem = new CompletionItem("Camel dependency for component " + componentModel.getTitle());
+			completionItem.setInsertText(
+					"<dependency>\n" +
+					"\t<groupId>"+componentModel.getGroupId()+"</groupId>\n" +
+					"\t<artifactId>"+componentModel.getArtifactId()+"</artifactId>\n" +
+					"</dependency>\n");
+			CompletionResolverUtils.applyDeprecation(completionItem, componentModel.getDeprecated());
+			completions.add(completionItem);
+		}
+		return completions;
+	}
+
+	private CompletionItem createCamelQuarkusDebugProfileCompletionItem() {
 		CompletionItem completionQuarkusDebugProfile = new CompletionItem("Camel debug profile for Quarkus");
 		completionQuarkusDebugProfile.setInsertText(
-				  "<profile>\n"
-				+ "    <id>camel.debug</id>\n"
-				+ "    <activation>\n"
-				+ "        <property>\n"
-				+ "            <name>camel.debug</name>\n"
-				+ "            <value>true</value>\n"
-				+ "        </property>\n"
-				+ "    </activation>\n"
-				+ "    <dependencies>\n"
-				+ "        <dependency>\n"
-				+ "            <groupId>org.apache.camel.quarkus</groupId>\n"
-				+ "            <artifactId>camel-quarkus-debug</artifactId>\n"
-				+ "        </dependency>\n"
-				+ "    </dependencies>\n"
-				+ "</profile>");
+				"""
+					<profile>
+					\t<id>camel.debug</id>
+					\t<activation>
+					\t\t<property>
+					\t\t\t<name>camel.debug</name>
+					\t\t\t<value>true</value>
+					\t\t</property>
+					\t</activation>
+					\t<dependencies>
+					\t\t<dependency>
+					\t\t\t<groupId>org.apache.camel.quarkus</groupId>
+					\t\t\t<artifactId>camel-quarkus-debug</artifactId>
+					\t\t</dependency>
+					\t</dependencies>
+					</profile>""");
 		completionQuarkusDebugProfile.setDocumentation(
-				"Adding a profile to enable Camel Debug.\n"
-				+ "Combined with launch configuration and tasks, it allows single-click debug.\n"
-				+ "It requires Camel Quarkus 2.14+");
-		return CompletableFuture.completedFuture(Collections.singletonList(completionQuarkusDebugProfile));
+				"""
+					Adding a profile to enable Camel Debug.
+					Combined with launch configuration and tasks, it allows single-click debug.
+					It requires Camel Quarkus 2.14+""");
+		return completionQuarkusDebugProfile;
+	}
+
+	/**
+	 * /!\ This can lead to some false positive but I think this is not really important for now.
+	 * 
+	 * @param position
+	 * @param lines
+	 * @param enclosingTag
+	 * @return
+	 */
+	private boolean isInsideTag(Position position, String[] lines, String enclosingTag) {
+		boolean hasEnclosingTagBefore = false;
+		boolean hasEnclosingTagAfter = false;
+		for (int i = position.getLine(); i >= 0 && i < lines.length; i--) {
+			if (lines[i].contains("<"+enclosingTag+">")) {
+				hasEnclosingTagBefore = true;
+				break;
+			}
+			if (lines[i].contains("</"+enclosingTag+">")) {
+				return false;
+			}
+		}
+		
+		for (int i = position.getLine(); i < lines.length; i++) {
+			if (lines[i].contains("</"+enclosingTag+">")) {
+				hasEnclosingTagAfter = true;
+				break;
+			}
+		}
+		
+		return hasEnclosingTagBefore && hasEnclosingTagAfter;
 	}
 
 }
